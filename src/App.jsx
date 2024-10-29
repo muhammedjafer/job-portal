0f6fefa7737a4e7a8f05e4940996eb5af411a35d
muhammedjafer <mohammadjafer990@gmail.com>
2024-10-29 13:18:16 +0300

    Setting up the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import HomeCards from './components/HomeCards';
-import JobListings from "./components/JobListings";
+import { 
+    Route, 
+    createBrowserRouter, 
+    createRoutesFromElements, 
+    RouterProvider 
+} 
+from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />} >
+            <Route index element={<HomePage />} />
+        </Route>
+    )
+);
 
 const App = () => {
-    return (
-        <>
-            <Navbar />
-            <Hero title='Test title' subtitle='this is the subtitle' />
-            <HomeCards />
-            <JobListings />
-            
-
-            <section className="m-auto max-w-lg my-10 px-6">
-                <a href="jobs.html" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
-                    View All Jobs
-                </a>
-            </section>
-        </>
-    );
+    return <RouterProvider router={router} />;
 };
 
 export default App;
